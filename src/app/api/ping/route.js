f01bdad7995eb362a0836ec7a26be94521981c56
sarrthak008 <sarthaknavale08@gmail.com>
2025-10-29 07:01:51 +0530

    add a cors for ping api

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,5 +1,16 @@
 import responder from "../../../../utils/responder.js"
 
+
+
+function corsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+}
+
+
 export const GET = async(req)=>{
       try {
         return responder("Server api is running healthy",null,200,true);
@@ -8,3 +19,10 @@ export const GET = async(req)=>{
       }
       
   }
+
+
+  // OPTIONS handler for preflight
+  export async function OPTIONS() {
+    return new Response(null, { status: 200, headers: corsHeaders() });
+  }
+  
